5e4be72e79b47c97303426aea404beb22391aa80
Kaleb-Dimitrio <145956341+Kaleb-Dimitrio@users.noreply.github.com>
2024-10-08 21:37:26 +0700

    Update and rename welcome.blade.php to home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,100&display=swap" rel="stylesheet">
+
+    <!-- Feather Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+<!-- Navbar -->
+    <nav class = "navbar">
+    <a href="#" class="navbar-logo">Kopi<span>Kita</span></a>
+
+    <div class="navbar-mid">
+    <a href="#">Home</a>
+    <a href="#">Menu</a>
+    <a href="#">Location</a>
+    </div>
+
+    <div class="navbar-right">
+    <a href="#" id = "login"><i data-feather = "user"></i></a>
+    </div>
+
+
+    
+    </nav>
+
+<!-- feather Icons   -->
+<script>
+      feather.replace();
+    </script>
+</body>
+</html>
